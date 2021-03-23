b2e93bf086639a1dc936faee4227c66866999519
AlexMcGil <therycb1337@gmail.com>
2021-03-23 23:32:51 +0300

    released with randint function

--- quicksort.py
+++ quicksort.py
@@ -3,16 +3,18 @@ n = int(input())
 m = []
 for i in range(n):
     m.append(random.randint(1, 20))
-l, e, g = [], [], []
-
-op = m[0]
-for i in m:
-    if i < op:
-        l.append(i)
-    elif i == op:
-        e.append(i)
+def qsort(m):
+    l, e, g = [], [], []
+    if len(m) > 1:
+        op = m[0]
+        for i in m:
+            if i < op:
+                l.append(i)
+            elif i == op:
+                e.append(i)
+            else:
+                g.append(i)
+        return qsort(l)+e+qsort(g)
     else:
-        g.append(i)
-ans=l+e+g
-print(m)
-print(ans)
+        return m
+print(qsort(m))
