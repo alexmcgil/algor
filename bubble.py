60ce10181f0191d1ac688dc16710ad3a5158a16b
AlexMcGil <therycb1337@gmail.com>
2021-03-23 22:11:43 +0300

    returned function randint

--- bubble.py
+++ bubble.py
@@ -2,7 +2,7 @@ import random
 n = int(input())
 m = []
 for i in range(n):
-    m.append(random.randint(1,20))
+    m.append(random.randint(1, 20))
 for k in m:
     for i in range(n-1):
         if m[i] > m[i+1]:
