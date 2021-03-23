b1f18174d651ae6566c79b3b3c08f0b1b068fd1c
AlexMcGil <therycb1337@gmail.com>
2021-03-23 22:08:09 +0300

    remove randint and use mas=[5, 1, 4, 2, 8]

--- bubble.py
+++ bubble.py
@@ -1,4 +1,10 @@
-import random
-m = []
-for i in range(25):
-    m.append(random.randint(1,20))
+#import random
+#n = int(input())
+m = [5, 1, 4, 2, 8]
+#for i in range(n):
+#    m.append(random.randint(1,20))
+for k in m:
+    for i in range(4):
+        if m[i] > m[i+1]:
+            m[i], m[i+1] = m[i+1], m[i]
+print(m)
