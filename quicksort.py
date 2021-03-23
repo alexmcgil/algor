0308dfe5fb67177620fc28f74677f168ec503988
AlexMcGil <therycb1337@gmail.com>
2021-03-23 23:22:57 +0300

    возможные варианты покупки энергетиков на сумму равную s

--- quicksort.py
+++ quicksort.py
@@ -1,5 +1,18 @@
 import random
+n = int(input())
 m = []
-for i in range(25):
-    m.append(random.randint(1,20))
+for i in range(n):
+    m.append(random.randint(1, 20))
+l, e, g = [], [], []
 
+op = m[0]
+for i in m:
+    if i < op:
+        l.append(i)
+    elif i == op:
+        e.append(i)
+    else:
+        g.append(i)
+ans=l+e+g
+print(m)
+print(ans)
